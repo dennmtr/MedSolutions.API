3e475aab8a34784c9ceb325b847042696816b75d
Dionisis Mitropoulos <dennmtr@gmail.com>
2025-10-06 13:22:42 +0300

    login request: remove error messages

--- MedSolutions.App/DTOs/LoginRequestDTO.cs
+++ MedSolutions.App/DTOs/LoginRequestDTO.cs
@@ -4,13 +4,13 @@ namespace MedSolutions.App.DTOs;
 
 public class LoginRequestDTO
 {
-    [MaxLength(256, ErrorMessage = "error.validation.email_length")]
-    [Required(ErrorMessage = "error.validation.email_required")]
-    [EmailAddress(ErrorMessage = "error.validation.email_format")]
+    [MaxLength(256)]
+    [Required]
+    [EmailAddress]
     public required string Email { get; set; }
 
-    [Required(ErrorMessage = "error.validation.password_required")]
-    [StringLength(128, ErrorMessage = "error.validation.password_length", MinimumLength = 6)]
+    [Required]
+    [StringLength(128, MinimumLength = 6)]
     public required string Password { get; set; }
-    public bool RememberMe { get; set; }
+    public bool? RememberMe { get; set; }
 }
